5958bffe78c3cb42f49eda5e8e69e6422f6b374f
Daniel Duarte <danielduarte@gmail.com>
2021-07-28 11:10:15 -0300

    listagem de nomes no node

---
+++ README.md
@@ -0,0 +1,3 @@
+docker run -it --rm --name node -p 3000:3000 --network fullcycle-docker-node_node-network danielduartecs/node-web-app bash
+
+build . -t danielduartecs/node-web-app
--- node/index.js
+++ node/index.js
@@ -2,33 +2,46 @@ const express = require('express')
 const mysql = require('mysql')
 const app = express()
 const port = 3000
+var nomes = []
 
-const config = {
+const db = mysql.createConnection({
     host: 'db',
     user: 'root',
     password: 'root',
     database: 'nodedb'
-}
+})
 
-const connection = mysql.createConnection(config)
+db.query("CREATE TABLE IF NOT EXISTS pessoas (id INT NOT NULL AUTO_INCREMENT, nome CHAR(30) NOT NULL, PRIMARY KEY (id));", function (err, results, fields) {
+    db.query("SELECT COUNT(*) AS total FROM pessoas;", function (err, results, fields) {
+        if (results[0].total == 0) {
+            db.query("INSERT INTO pessoas (nome) VALUES ('Daniel'),('Maria'),('Francisco'),('Sandra');", function (err, results, fields) {
+                db.query("SELECT nome FROM pessoas;", function (err, results, fields) {
+                    nomes = results;
+                    db.end()
+                });
+            })
+        }
+        else {
+            db.query("SELECT nome FROM pessoas;", function (err, results, fields) {
+                nomes = results;
+                db.end()
+            });
+        }
+    })
+})
 
 
-connection.query("CREATE TABLE IF NOT EXISTS pessoas (id INT NOT NULL AUTO_INCREMENT, nome CHAR(30) NOT NULL, PRIMARY KEY (id));")
-
-connection.query('SELECT count(*) AS total FROM pessoas', function(err, rows, fields) {
-    if (err) throw err;
-    if( rows[0].total == 0 ) {
-        connection.query("INSERT INTO pessoas(nome) values('Daniel', 'Maria', 'Bernado', 'Samara')")
-    }
-});
-
-
-connection.end()
-
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cylcle 2.0</h1>')
+app.get('/', (req, res) => {
+    resultado = `<h1>Full Cylcle 2.0</h1><br /><br />
+    <h2>Nomes cadastrados:</h2><br />
+    <ul>`
+    Object.keys(nomes).forEach(function (key) {
+        resultado = resultado + `<li>${nomes[key].nome}</li>`
+    });
+    resultado = resultado + `</ul>`
+    res.send(resultado)
 })
 
 app.listen(port, () => {
-    console.log('Rodando na porta ' + port)
+    console.log('Servidor executando na porta: ' + port)
 })
